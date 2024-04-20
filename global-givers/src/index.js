ee62fb34a957c281a08f220353334b8cf5e7775c
Gautham V <gauthamv@Gauthams-MacBook-Air.local>
2024-04-20 01:29:20 -0400

    Minor Changes

--- global-givers/src/index.js
+++ global-givers/src/index.js
@@ -9,7 +9,9 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
